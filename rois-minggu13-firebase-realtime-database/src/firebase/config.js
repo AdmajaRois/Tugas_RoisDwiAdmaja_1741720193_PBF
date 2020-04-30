a9b48fc76b6adaa439bc8af9d6a1acacb6276e94
AdmajaRois <rois.da.silva@gmail.com>
2020-04-30 07:20:39 +0700

    melakukan bakcup copas file src

---
+++ rois-minggu13-firebase-realtime-database/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCsFkorNs1_GJOIYRr0zzpAx8GZowxtYH8",
+    authDomain: "react-login-fab26.firebaseapp.com",
+    databaseURL: "https://react-login-fab26.firebaseio.com",
+    projectId: "react-login-fab26",
+    storageBucket: "react-login-fab26.appspot.com",
+    messagingSenderId: "506362191682",
+    appId: "1:506362191682:web:1a4e23451f9ca5bf09f2db",
+    measurementId: "G-MVQPVSD6K1"
+  };
+
+  export default firebaseConfig;
