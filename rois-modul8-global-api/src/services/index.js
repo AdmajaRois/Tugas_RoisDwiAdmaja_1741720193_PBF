11f6c162a0e81e71790866f81f80276d2ae51b9a
AdmajaRois <rois.da.silva@gmail.com>
2020-04-30 07:08:51 +0700

    melakukan backup dan memindahkan file src dan public

--- rois-modul8-global-api/src/services/index.js
+++ rois-modul8-global-api/src/services/index.js
@@ -1,29 +1,19 @@
-const domainPatch = 'http://localhost:3500';
-const GetAPI = (path) =>{
-    const promise = new Promise((resolve, reject)=>{
-        fetch(`${domainPath}/${path}`, {
-            method: 'post',
-            headers: {
-                'Accept' : 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body:JSON.stringify(data)
-        })
-            .then((result)=>{
-                resolve(result);
-            }, (err)=>{
-                reject(err);
-            })
-    })
-    return promise;
-}
+import GetAPI from "./Get";
+import PostAPI from "./Post";
+import DeleteAPI from "./Delete";
 
-const getNewsBlog = () => GetAPI('posts?_sort=id&_order=desc');
-const postNewsBlog = () => PostAPI('posts', dataYDikirim);
+// Daftar API - GET
+const getNewsBlog  = () => GetAPI('posts?_sort=id&_order=desc');
 
-const API = {
+// Daftar API - POST
+const postNewsBlog = (dataYgDiKirim) => PostAPI('posts', dataYgDiKirim);
+
+// Daftar API - DELETE
+const deleteNewsBlog = (dataYgDiHapus) => DeleteAPI('posts', dataYgDiHapus);
+
+const API = {       // inisialisasi function-function yang akan disediakan global API.
     getNewsBlog,
-    postNewsBlog
+    postNewsBlog,
+    deleteNewsBlog
 }
-
 export default API;
