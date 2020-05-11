3969812ee28e10b43e6a4fc914ce174ed4d22c8d
AdmajaRois <rois.da.silva@gmail.com>
2020-05-11 13:59:26 +0700

    initialize uas

---
+++ rois-uas/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDYzJDc_094Ojqzx2K_bXuTcjgG3zE-xuk",
+    authDomain: "movieapp-4130e.firebaseapp.com",
+    databaseURL: "https://movieapp-4130e.firebaseio.com",
+    projectId: "movieapp-4130e",
+    storageBucket: "movieapp-4130e.appspot.com",
+    messagingSenderId: "294019740893",
+    appId: "1:294019740893:web:6f471d264e51500a7c1734",
+    measurementId: "G-EP42ST46YX"
+};
+
+export default firebaseConfig;
